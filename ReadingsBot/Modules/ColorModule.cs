7be17f7a520c2c8321f15664ba8a6a101613ad41
Felipe C. R. Miftajov <koslink@gmail.com>
2021-04-11 18:22:17 -0500

    Added version info to embeds
    - Any embeds related to help or info for ReadingsBot will show the bot
      version
    - Default colors for embeds are now set using an interface

---
+++ ReadingsBot/Modules/ColorModule.cs
@@ -0,0 +1,17 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace ReadingsBot.Modules
+{
+    public abstract class ColorModule : ModuleBase<SocketCommandContext>
+    {
+        protected Color EmbedColor { get; }
+
+        protected ColorModule(Color color)
+        {
+            EmbedColor = color;
+        }
+
+        protected virtual EmbedBuilder BasicEmbedBuilder => new EmbedBuilder().WithColor(EmbedColor);
+    }
+}
