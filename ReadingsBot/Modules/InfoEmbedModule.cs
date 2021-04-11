2413e2172dbf33d68b2e6ae3b1a45864bfff533c
Felipe C. R. Miftajov <koslink@gmail.com>
2021-04-11 18:22:17 -0500

    Added version info to embeds
    - Any embeds related to help or info for ReadingsBot will show the bot
      version
    - Default colors for embeds are now set using an interface

---
+++ ReadingsBot/Modules/InfoEmbedModule.cs
@@ -0,0 +1,17 @@
+﻿using Discord;
+
+namespace ReadingsBot.Modules
+{
+    public abstract class InfoEmbedModule : ColorModule
+    {
+        protected readonly ReadingsBotVersionInfo _versionInfo;
+
+        protected InfoEmbedModule(ReadingsBotVersionInfo versionInfo, Color color)
+            : base(color)
+        {
+            _versionInfo = versionInfo;
+        }
+
+        protected override EmbedBuilder BasicEmbedBuilder => base.BasicEmbedBuilder.WithAuthor(_versionInfo.Header);
+    }
+}
---
+++ ReadingsBot/Modules/ReadingsBotVersionInfo.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System.Reflection;
+
+namespace ReadingsBot
+{
+    public class ReadingsBotVersionInfo
+    {
+        protected readonly string name = Assembly.GetExecutingAssembly().GetName().Name;
+        protected readonly string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        private readonly DiscordSocketClient _client;
+
+        public string Name => name;
+        public string Version => version;
+
+        public EmbedAuthorBuilder Header => new EmbedAuthorBuilder()
+            .WithName($"{name} {version}")
+            .WithIconUrl(_client.CurrentUser.GetAvatarUrl());
+
+        public ReadingsBotVersionInfo(DiscordSocketClient client)
+        {
+            _client = client;
+        }
+    }
+}
